3eb2d4351097b2e113962a482b93381e059d1ce3
SHRESHTA ANUGU <131268085+shre-45@users.noreply.github.com>
2025-10-08 18:25:37 +0530

    Update variables.tf

--- terraform-root/modules/service-principal/variables.tf
+++ terraform-root/modules/service-principal/variables.tf
@@ -2,3 +2,7 @@ variable "service_principal_name" {
   description = "Display name for the Service Principal"
   type        = string
 }
+variable "subscription_id" {
+  description = "Target Azure subscription ID"
+  type        = string
+}
